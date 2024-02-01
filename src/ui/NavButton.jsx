a5bdd322a78b31fc84ae5f8056a48c5eb22ef6ba
ShekeemJoseph <shakeemjoseph.tosl@gmail.com>
2024-01-31 21:08:17 -0400

    Updated page for series/movie title

--- src/ui/NavButton.jsx
+++ src/ui/NavButton.jsx
@@ -15,9 +15,6 @@ const StyledNavLink = styled(NavLink)`
     transition: all 0.3s;
     border-radius: var(--border-radius-sm);
   }
-  &.active {
-    pointer-events: none;
-  }
   /* This works because react-router places the active class on the active NavLink */
   &:hover,
   &:active,
